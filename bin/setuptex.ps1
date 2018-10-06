aec78b63f8c5dbd520447b8337e800d3d4f809d4
Wader <wader@live.com>
2018-10-07 01:02:27 +0800

    Add ConTeXt Env setup script.

---
+++ bin/setuptex.ps1
@@ -0,0 +1,55 @@
+# Author: Wader Chan
+
+Param(
+    [string]$TexHome
+)
+
+function Get-TEXMFOS([string]$TexHome)
+{
+    $TexHome=$TexHome.TrimEnd("\")
+
+    $TEXMFOS="$TexHome\texmf-mswin-64"
+    if (Test-Path "$TEXMFOS\bin\mtxrun.exe") { return $TEXMFOS }
+
+    $TEXMFOS="$TexHome\texmf-win64"
+    if (Test-Path "$TEXMFOS\bin\mtxrun.exe") { return $TEXMFOS }
+
+    $TEXMFOS="$TexHome\texmf-mswin"
+    if (Test-Path "$TEXMFOS\bin\mtxrun.exe") { return $TEXMFOS }
+
+    $TEXMFOS="$TexHome\texmf-win32"
+    if (Test-Path "$TEXMFOS\bin\mtxrun.exe") { return $TEXMFOS }
+
+    return $NULL
+}
+
+if ("" -eq $TexHome) {
+    $script:TexHome="D:\SDK\context\tex"
+}
+
+$TEXMFOS = Get-TEXMFOS $TexHome
+
+if ($NULL -ne $TEXMFOS ) {
+    $env:PATH="$TEXMFOS\bin;"+$env:PATH
+    $env:CTXMINIMAL="yes"
+    Write-Host "Setting ConTeXt Env successful."
+} else {
+    Write-Host "Setting ConTeXt Env failed."
+}
+
+function global:Exec-Cmd([string]$cmd) {
+    Write-Host $cmd
+    Invoke-Expression $cmd
+}
+
+function global:Ctx-FontsReload {
+    Exec-Cmd "mtxrun --script fonts --reload"
+}
+
+function global:Ctx-FontsList {
+    Exec-Cmd "mtxrun --script fonts --list --all"
+}
+
+function global:Ctx-LuatoolsGenerate {
+    Exec-Cmd "luatools --generate"    
+}
